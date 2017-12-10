9843beffe5c245e4b234ab09f4aa02745b2a4f62
manish kumar <manish kumar>
2017-12-10 22:48:36 +0530

    AJAX sample code

---
+++ script.js
@@ -0,0 +1,10 @@
+// Sending normal GET request to server. (SYNC request)
+var request = new XMLHttpRequest();
+request.open('GET','data.txt',false);
+request.send();
+if(request.status === 200){
+console.log(request);
+    document.writeln(request.responseText);
+}
+
+//
